2618c2355f1024dc8b3e932d4776cb520c214d6b
Florian Speidel <florian.speidel@xing.com>
2012-04-27 11:23:34 +0200

    added support for components

--- lib/pool.rb
+++ lib/pool.rb
@@ -6,14 +6,14 @@ class Pool
     @category = ["stage", "pool", "production"]
   end
 
-  def setup(suitename)
-    if not allowed_suites.include?(suitename)
-      puts "suitename is not configured"
-      exit 0
-    end
+  def setup(suitename, component)
+    # if not allowed_suites.include?(suitename)
+    #   puts "suitename is not configured"
+    #   exit 0
+    # end
 
     @category.each do |name|
-      directory = File.join(@config.get[:rootdir], name, suitename)
+      directory = File.join(@config.get[:rootdir], name, suitename, component)
 
       unless Dir.exists?(directory)
         FileUtils.mkdir_p(directory)
@@ -21,35 +21,42 @@ class Pool
     end
   end
 
-  def pool_dir(suitename)
-    File.join(@config.get[:rootdir], "pool", suitename)
+  def pool_dir(suitename, component)
+    File.join(@config.get[:rootdir], "pool", suitename, component)
   end
 
-  def stage_dir(suitename)
-    File.join(@config.get[:rootdir], "stage", suitename)
+  def stage_dir(suitename, component)
+    File.join(@config.get[:rootdir], "stage", suitename, component)
   end
 
-  def production_dir(suitename)
-    File.join(@config.get[:rootdir], "production", suitename)
+  def production_dir(suitename, component)
+    File.join(@config.get[:rootdir], "production", suitename, component)
   end
 
-  def allowed_suites
-    suites = []
-    @config.get[:suites].each do |name|
-      suites << name unless suites.include?(name)
+  # def allowed_suites
+  #   suites = []
+  #   @config.get[:suites].each do |name|
+  #     suites << name unless suites.include?(name)
+  #   end
+  #   suites
+  # end
+
+  def structure
+    structures = {}
+    Dir.glob(File.join(@config.get[:rootdir], "stage", "*")).each do |suitedir|
+      components = []
+      suite = File.split(suitedir)
+
+      Dir.glob(File.join(@config.get[:rootdir], "stage", suite[1], "*")).each do |componentdir|
+        component = File.split(componentdir)
+        components << component[1] unless components.include?(component[1])
+      end
+      structures[suite[1]] = components unless structures.has_key?(suite[1])
     end
-    suites
-  end
-
-  def active_suites
-    suitedir = Dir.glob(File.join(@config.get[:rootdir], "*", "*"))
-    suites   = []
-
-    suitedir.each do |name|
-      basename = File.basename(name)
-      suites << basename unless suites.include?(basename)
-    end
-    suites
+    structures
   end
 end
+
+
+
 
--- lib/repomate.rb
+++ lib/repomate.rb
@@ -12,41 +12,82 @@ class RepoMate
     @config = Configuration.new
   end
 
-  def stage(source_fullname, suitename)
+  def stage(source_fullname, suitename, component)
     package              = Package.new(source_fullname, suitename)
-    destination_fullname = File.join(pool.stage_dir(suitename), package.newbasename)
+    destination_fullname = File.join(pool.stage_dir(suitename, component), package.newbasename)
 
-    pool.setup(suitename)
+    pool.setup(suitename, component)
 
-    puts "Package: #{package.newbasename} moved to stage/#{suitename}"
+    puts "Package: #{package.newbasename} moved to stage => #{suitename}/#{component}"
 
     FileUtils.copy(source_fullname, destination_fullname)
   end
 
   def publish
-    pool.active_suites.each do |suitename|
-      debfiles = File.join(pool.stage_dir(suitename), "*.deb")
+    pool.structure.each do |suitename, components|
+      components.each do |component|
+        debfiles = File.join(pool.stage_dir(suitename, component), "*.deb")
 
-      Dir.glob(debfiles) do |source_fullname|
-        package              = Package.new(source_fullname, suitename)
-        destination_fullname = File.join(pool.pool_dir(suitename), package.newbasename)
+        Dir.glob(debfiles) do |source_fullname|
+          package              = Package.new(source_fullname, suitename)
+          destination_fullname = File.join(pool.pool_dir(suitename, component), package.newbasename)
 
-        FileUtils.move(source_fullname, destination_fullname)
+          FileUtils.move(source_fullname, destination_fullname)
 
-        source_fullname = destination_fullname
+          source_fullname = destination_fullname
 
-        link(source_fullname, pool.production_dir(suitename), suitename)
+          link(source_fullname, pool.production_dir(suitename, component), suitename)
+        end
       end
+    end
+  end
+
+  def link(source_fullname, destination_dir, suitename)
+    source_package = Package.new(source_fullname, suitename)
+    source_version = source_package.controlfile['Version']
+    debfiles       = "#{destination_dir}/#{source_package.controlfile['Package']}*.deb"
+    action         = 1
+    dpkg           = @config.get[:dpkg]
+
+    raise "dpkg is not installed" unless File.exists?(dpkg)
+
+    Dir.glob(debfiles) do |destination_fullname|
+      destination_package = Package.new(destination_fullname, suitename)
+      destination_version = destination_package.controlfile['Version']
+
+      if system("#{dpkg} --compare-versions #{source_version} gt #{destination_version}")
+        puts "Package: #{destination_package.newbasename} replaced with #{source_package.newbasename}."
+        File.unlink(destination_fullname)
+      elsif system("#{dpkg} --compare-versions #{source_version} eq #{destination_version}")
+        puts "Package: #{source_package.newbasename} already exists with same version."
+        action = nil
+      elsif system("#{dpkg} --compare-versions #{source_version} lt #{destination_version}")
+        puts "Package: #{source_package.newbasename} already exists with higher version."
+        action = nil
+      end
+    end
+
+    if not action.nil?
+      save_checkpoint
+
+      destination_fullname = File.join(destination_dir, source_package.newbasename)
+      puts "Package: #{source_package.newbasename} linked to production/#{suitename}"
+
+      File.symlink(source_fullname, destination_fullname)
+
+      # scan_packages
     end
   end
 
   def save_checkpoint
     File.open(@config.get[:redolog], 'a') do |file|
-      pool.active_suites.each do |suitename|
-        debfiles = File.join(pool.production_dir(suitename), "*.deb")
-        Dir.glob(debfiles) do |fullname|
-          basename = File.basename(fullname)
-          file.puts "#{DateTime.now} #{suitename} #{basename}"
+    pool.structure.each do |suitename, components|
+      components.each do |component|
+          debfiles = File.join(pool.production_dir(suitename, component), "*.deb")
+          Dir.glob(debfiles) do |fullname|
+            basename = File.basename(fullname)
+            file.puts "#{DateTime.now} #{suitename}/#{component} #{basename}"
+          end
         end
       end
     end
@@ -94,10 +135,12 @@ Everything between the last two \"unstage (-u) commands\" will be lost if you pr
       STDERR.puts "Invalid number"
       exit 0
     else
-      pool.active_suites.each do |suitename|
-        debfiles = File.join(pool.production_dir(suitename), "*.deb")
-        Dir.glob(debfiles) do |fullname|
-          File.unlink fullname
+      pool.structure.each do |suitename, components|
+        components.each do |component|
+          debfiles = File.join(pool.production_dir(suitename, component), "*.deb")
+          Dir.glob(debfiles) do |fullname|
+            File.unlink fullname
+          end
         end
       end
 
@@ -107,58 +150,62 @@ Everything between the last two \"unstage (-u) commands\" will be lost if you pr
         while (line = file.gets)
           if line.split[0] == list[number]
             basename     = line.split[2]
-            suitename    = line.split[1]
-            poolbasename = File.join(pool.pool_dir(suitename), basename)
+            structure    = line.split[1]
+            poolbasename = File.join(pool.pool_dir(structure), basename)
 
-            link(poolbasename, pool.production_dir(suitename), suitename)
+            link(poolbasename, pool.production_dir(structure), suitename)
           end
         end
       end
-      scan_packages
+      # scan_packages
     end
   end
 
   def scan_packages
   # TODO: systemcall or better gem for digest stuff
-    pool.active_suites.each do |suitename|
-      packages    = File.join(pool.production_dir(suitename), "Packages")
-      packages_gz = File.join(pool.production_dir(suitename), "Packages.gz")
-      debfiles    = File.join(pool.production_dir(suitename), "*.deb")
+    pool.structure.each do |suitename, components|
+      components.each do |component|
+        packages    = File.join(pool.production_dir(suitename, component), "Packages")
+        packages_gz = File.join(pool.production_dir(suitename, component), "Packages.gz")
+        debfiles    = File.join(pool.production_dir(suitename, component), "*.deb")
 
-      File.unlink(packages_gz) if File.exists?(packages_gz)
+        File.unlink(packages_gz) if File.exists?(packages_gz)
 
-      Dir.glob(debfiles) do |fullname|
-        package = Package.new(fullname, suitename)
+        Dir.glob(debfiles) do |fullname|
+          package = Package.new(fullname, suitename)
 
-        File.open(packages, 'a') do |file|
-          package.controlfile.each do |key, value|
-            file.puts "#{key}: #{value}"
+          File.open(packages, 'a') do |file|
+            package.controlfile.each do |key, value|
+              file.puts "#{key}: #{value}"
+            end
+            file.puts "MD5sum: #{Digest::MD5.file(fullname).to_s}"
+            file.puts "SHA1: #{Digest::SHA1.file(fullname).to_s}"
+            file.puts "SHA256: #{Digest::SHA256.new(256).file(fullname).to_s}\n\n"
           end
-          file.puts "MD5sum: #{Digest::MD5.file(fullname).to_s}"
-          file.puts "SHA1: #{Digest::SHA1.file(fullname).to_s}"
-          file.puts "SHA256: #{Digest::SHA256.new(256).file(fullname).to_s}\n\n"
         end
-      end
-      if File.exists?(packages)
-        raise "Could not gzip" unless system "gzip -9 -c #{packages} >> #{packages_gz}"
+        if File.exists?(packages)
+          raise "Could not gzip" unless system "gzip -9 -c #{packages} >> #{packages_gz}"
+        end
       end
     end
   end
 
   def list_packages(*suitename)
     if suitename.empty?
-      pool.active_suites.each do |suitename|
-        list_packages_by_suite(suitename)
+      pool.structure.each do |suitename, components|
+        components.each do |component|
+          list_packages_by_suite(suitename, component)
+        end
       end
     else
-      list_packages_by_suite(suitename)
+      list_packages_by_suite(suitename, component)
     end
   end
 
-  def list_packages_by_suite(suitename)
+  def list_packages_by_suite(suitename, component)
     debfiles = File.join(pool.pool_dir(suitename), "*.deb")
 
-    puts "Active packages in #{suitename}:"
+    puts "Active packages in #{suitename}/#{component}:"
 
     Dir.glob(debfiles) do |source_fullname|
       package = Package.new(source_fullname, suitename)
@@ -175,42 +222,5 @@ Everything between the last two \"unstage (-u) commands\" will be lost if you pr
 
   def pool
     @pool ||= Pool.new
-  end
-
-  def link(source_fullname, destination_dir, suitename)
-    source_package = Package.new(source_fullname, suitename)
-    source_version = source_package.controlfile['Version']
-    debfiles       = "#{destination_dir}/#{source_package.controlfile['Package']}*.deb"
-    action         = 1
-    dpkg           = @config.get[:dpkg]
-
-    raise "dpkg is not installed" unless File.exists?(dpkg)
-
-    Dir.glob(debfiles) do |destination_fullname|
-      destination_package = Package.new(destination_fullname, suitename)
-      destination_version = destination_package.controlfile['Version']
-
-      if system("#{dpkg} --compare-versions #{source_version} gt #{destination_version}")
-        puts "Package: #{destination_package.newbasename} replaced with #{source_package.newbasename}."
-        File.unlink(destination_fullname)
-      elsif system("#{dpkg} --compare-versions #{source_version} eq #{destination_version}")
-        puts "Package: #{source_package.newbasename} already exists with same version."
-        action = nil
-      elsif system("#{dpkg} --compare-versions #{source_version} lt #{destination_version}")
-        puts "Package: #{source_package.newbasename} already exists with higher version."
-        action = nil
-      end
-    end
-
-    if not action.nil?
-      save_checkpoint
-
-      destination_fullname = File.join(destination_dir, source_package.newbasename)
-      puts "Package: #{source_package.newbasename} linked to production/#{suitename}"
-
-      File.symlink(source_fullname, destination_fullname)
-
-      scan_packages
-    end
   end
 end
