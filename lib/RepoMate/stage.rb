cf22d6ad0903716fafca7cd2816b1f478ce6ffaa
Michael Ehrenreich <michael.ehrenreich@me.com>
2012-04-28 14:41:22 +0200

    added class for stage

---
+++ lib/RepoMate/stage.rb
@@ -0,0 +1,32 @@
+require_relative '../configuration'
+
+module RepoMate
+  class Stage
+
+    def initialize(stage)
+      @config     = Configuration.new
+      @stage      = stage
+    end
+
+    def directory
+      File.join(@config.get[:rootdir], @stage)
+    end
+
+    def exist?
+      Dir.exist?(directory)
+    end
+
+    def is_allowed?
+      @config.get[:components].include?(@stage)
+    end
+
+    def create
+      FileUtils.mkdir_p(directory) unless exist?
+    end
+
+    def destroy
+      FileUtils.rm_r(directory) if exist?
+    end
+
+  end
+end
