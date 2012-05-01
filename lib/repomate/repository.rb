8d8048ce40889160ed4b79101c34a20a17d2157c
Florian Speidel <flo@doobie.cc>
2012-05-01 14:46:31 +0200

    added repository class

---
+++ lib/repomate/repository.rb
@@ -0,0 +1,58 @@
+require_relative 'configuration'
+require_relative 'architecture'
+require_relative 'component'
+require_relative 'category'
+require_relative 'suite'
+
+module RepoMate
+  class Repository
+
+    def initialize
+      @config     = Configuration.new
+      @categories = ["stage", "pool", "dists"]
+    end
+
+    def create(suitename=nil, component=nil, architecture=nil)
+      unless Suite.allowed.include?(suitename)
+        $stderr.puts "Suitename is not configured"
+        exit 1
+      end
+      unless Component.allowed.include?(component)
+        $stderr.puts "Component is not configured"
+        exit 1
+      end
+
+      @categories.each do |category|
+        unless suitename.nil?
+          Suite.new(suitename, category).create
+        end
+
+        unless component.nil? || suitename.nil?
+          Component.new(component, suitename, category).create
+        end
+
+        unless architecture.nil? || component.nil? || suitename.nil?
+          Architecture.new(architecture, component, suitename, category).create
+        end
+      end
+    end
+
+    def loop(category)
+      structures   = []
+
+      Dir.glob(File.join(@config.get[:rootdir], category, "*")).each do |suitedir|
+        suitename = File.split(suitedir)
+        Dir.glob(File.join(@config.get[:rootdir], category, suitename[1], "*")).each do |componentdir|
+          component = File.split(componentdir)
+          Dir.glob(File.join(@config.get[:rootdir], category, suitename[1], component[1], "*")).each do |architecturedir|
+            architecture_dir = File.split(architecturedir)
+            architecture = architecture_dir[1].split("-")
+            structures << { :suitename => suitename[1], :component => component[1], :architecture_dir => architecture_dir[1], :architecture => architecture[1]}
+          end
+        end
+      end
+      structures
+    end
+  end
+end
+
